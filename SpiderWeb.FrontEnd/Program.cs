6bd4cd1b6d594006b1b8db3b99e6123a82c34e62
emmanuel-torres <emmanuel.torres@junipersys.com>
2022-08-16 14:00:38 -0600

    I am able to talk between containers

--- SpiderWeb.FrontEnd/Program.cs
+++ SpiderWeb.FrontEnd/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+Console.WriteLine(builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
--- SpiderWeb.LicensingApi/Controllers/WeatherForecastController.cs
+++ SpiderWeb.LicensingApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SpiderWeb.LicensingApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,7 +18,7 @@ namespace SpiderWeb.LicensingApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("weather")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
